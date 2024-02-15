d7707d26929added425b96a7be1a4e4af6ad2fe3
Kalyani Kishor Shinde <47003107+fresherscoding@users.noreply.github.com>
2024-02-16 02:29:04 +0530

    Removed older code and pushing simple code.

---
+++ simple-code/src/main/java/com/example/SimpleCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCodeApplication.class, args);
+	}
+
+}
